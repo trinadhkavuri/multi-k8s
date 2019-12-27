304f65297523b1e33f49908a7f41b66a8c3b9c2d
Ubuntu <tkavuri@DEMO1.if5hsgjppqpuxkb2mly50mgl3b.bx.internal.cloudapp.net>
2019-12-27 19:32:23 +0000

    jjh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,16 @@
 docker build -t tkavuri/multi-client:latest -t tkavuri/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t tkavuri/multi-api:latest -t tkavuri/multi-api:$SHA -f ./server/Dockerfile ./server
+docker build -t tkavuri/multi-api:latest -t tkavuri/multi-api:$SHA -t tkavuri/multi-api:latest -t tkavuri/multi-api:$SHA -f ./server/Dockerfile ./server
 docker build -t tkavuri/multi-worker:latest -t tkavuri/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 
 docker push tkavuri/multi-client:latest
 docker push tkavuri/multi-api:latest
+docker push tkavuri/multi-server:latest
 docker push tkavuri/multi-worker:latest
 
 docker push tkavuri/multi-client:$SHA
 docker push tkavuri/multi-api:$SHA
+docker push tkavuri/multi-server:$SHA
 docker push tkavuri/multi-worker:$SHA
 
 kubectl apply -f k8s
