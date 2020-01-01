47e1345de793de106371c879ac22b324ab980dea
Ubuntu <tkavuri@DEMO1.if5hsgjppqpuxkb2mly50mgl3b.bx.internal.cloudapp.net>
2020-01-01 16:52:21 +0000

    test

--- server/index.js
+++ server/index.js
@@ -36,7 +36,7 @@ const redisPublisher = redisClient.duplicate();
 // Express route handlers
 
 app.get('/', (req, res) => {
-	#res.send('Hi');
+	//res.send('Hi');
   const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
